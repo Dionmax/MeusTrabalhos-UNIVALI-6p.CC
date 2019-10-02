6a09572756997873d2d22dfd35597ebe6cbec2cc
Dionmax <dionmaxfn@gmail.com>
2019-10-01 21:57:30 -0300

    Iniciando Grafos Matriz

--- Grafo_Trabalho_M2/Grafo_Trabalho_M2/GrafoMatriz.cpp
+++ Grafo_Trabalho_M2/Grafo_Trabalho_M2/GrafoMatriz.cpp
@@ -1,17 +1,29 @@
-class GrafoMatriz
+#pragma once
+#include <iostream>
+#include "Grafo.cpp"
+#include <iostream>
+#include <list>
+#include <algorithm>
+#include <stack> 
+#include <queue> 
+#include <functional>
+
+#define INFINITO 10000000
+
+using namespace std;
+
+class GrafoMatriz : public Grafo
 {
 public:
-	GrafoMatriz();
-	~GrafoMatriz();
+	GrafoMatriz() : Grafo()
+	{
 
-private:
+	}
 
-};
+	GrafoMatriz(int vertices, bool direcionado, bool ponderado)
+		: Grafo(vertices, direcionado, ponderado)
+	{
 
-GrafoMatriz::GrafoMatriz()
-{
-}
+	}
 
-GrafoMatriz::~GrafoMatriz()
-{
 }
--- Grafo_Trabalho_M2/Grafo_Trabalho_M2/Grafo_Trabalho_M2.cpp
+++ Grafo_Trabalho_M2/Grafo_Trabalho_M2/Grafo_Trabalho_M2.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 #include "Grafo.cpp"
 #include "GrafoLista.cpp"
+#include "GrafoMatriz.cpp"
 
 using namespace std;
 
 int main()
 {
-	GrafoLista grafolista = GrafoLista(5, false, false);
+	GrafoMatriz grafo = GrafoMatriz(5, false, false);
 
-	grafolista.InserirAresta(0, 1, 3);
-	grafolista.InserirAresta(0, 2, 5);
-	grafolista.InserirAresta(0, 3, 6);
-	grafolista.InserirAresta(0, 4, 8);
-	grafolista.InserirAresta(1, 3, 2);
-	grafolista.InserirAresta(1, 4, 11);
-	grafolista.InserirAresta(2, 4, 2);
-	grafolista.InserirAresta(3, 4, 1);
+
+	grafo.InserirAresta(0, 1, 3);
+	grafo.InserirAresta(0, 2, 5);
+	grafo.InserirAresta(0, 3, 6);
+	grafo.InserirAresta(0, 4, 8);
+	grafo.InserirAresta(1, 3, 2);
+	grafo.InserirAresta(1, 4, 11);
+	grafo.InserirAresta(2, 4, 2);
+	grafo.InserirAresta(3, 4, 1);
 
 	//cout << graforlista.ObterGrauDeSaida(0) << endl;
 	//cout << graforlista.ExisteVizinho(1, 3) << endl;
@@ -24,5 +26,5 @@ int main()
 	//cout << endl;
 	//graforlista.BuscaProfundidade(0);
 	//cout << endl;
-	cout << grafolista.Dijkstra(3, 0) << endl;
+	//cout << grafo.Dijkstra(3, 0) << endl;
 }
