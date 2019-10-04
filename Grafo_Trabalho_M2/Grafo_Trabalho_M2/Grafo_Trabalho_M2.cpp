cbffaf7a8d12edc708709c86d2c1eff3c50222f9
Dionmax <dionmaxfn@gmail.com>
2019-10-04 15:42:58 -0300

    Inserido método: leiruta de TXT -> Grafo

--- Grafo_Trabalho_M2/Grafo_Trabalho_M2/Grafo_Trabalho_M2.cpp
+++ Grafo_Trabalho_M2/Grafo_Trabalho_M2/Grafo_Trabalho_M2.cpp
@@ -1,29 +1,87 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <list>
+
 #include "Grafo.cpp"
 #include "GrafoLista.cpp"
 #include "GrafoMatriz.cpp"
 
 using namespace std;
 
+vector<string> split(string str, char delimiter = ' ')
+{
+	vector<string> ret;
+
+	int start = 0;
+
+	for (int i = 0; i < str.length(); ++i) {
+		if (str[i] == delimiter) {
+			if (i - start != 0)
+				ret.push_back(str.substr(start, i - start));
+			start = i + 1;
+		}
+	}
+
+	if (str.length() - start != 0)
+		ret.push_back(str.substr(start, start - str.length()));
+
+	return ret;
+}
+
 int main()
 {
 	//GrafoMatriz grafo = GrafoMatriz(5, false, true);
-	GrafoLista grafo = GrafoLista(5, false, true);
+	GrafoLista grafo;// = GrafoLista();
 
+	string path = "C:/Temp/trabalho-65cores1.txt";
+	ifstream texto(path);
 
-	grafo.InserirAresta(0, 1, 3);
-	grafo.InserirAresta(0, 2, 5);
-	grafo.InserirAresta(0, 3, 6);
-	grafo.InserirAresta(0, 4, 8);
-	grafo.InserirAresta(1, 3, 2);
-	grafo.InserirAresta(1, 4, 11);
-	grafo.InserirAresta(2, 4, 2);
-	grafo.InserirAresta(3, 4, 1);
+	string line = "";
 
+	int valoresHead[4] = { 0, 0, 0, 0 };
+	int valoresBody[2] = { 0 , 0 };
+
+	if (texto.is_open())
+	{
+		getline(texto, line);
+
+		int i = 0;
+
+		for (auto it : split(line, ' ')) {
+			valoresHead[i] = stoi(it);
+			i++;
+		}
+
+		i = 0;
+
+		grafo = GrafoLista(valoresHead[0], true && valoresHead[2], true && valoresHead[3]);
+
+		while (!texto.eof())
+		{
+			getline(texto, line);
+
+			for (auto it : split(line, ' ')) {
+				valoresBody[i] = stoi(it);
+				i++;
+			}
+
+			grafo.InserirAresta(valoresBody[0], valoresBody[1], 0);
+
+			i = 0;
+		}
+
+		texto.close();
+	}
+	else
+		cout << "Unable to open file";
+
+	//cout << grafo.getNumeroVertices();
+	//grafo.InserirAresta(0, 1, 3);
 	grafo.WelshPowell();
-
 	//cout << grafo.PesoAresta(0, 1);
-	//grafo.BuscaLargura(0);
+	//grafo.BuscaLargura(2);
 	//grafo.BuscaProfundidade(0);
 	//cout << grafo.ExisteVizinho(2, 0);
 	//cout << grafo.ObterGrauDeSaida(0);
