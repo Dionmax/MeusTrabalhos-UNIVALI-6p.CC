a78594be6ae3ac466f691c451089d7a8cc66c61d
Dionmax <dionmaxfn@gmail.com>
2019-10-25 19:57:26 -0300

    Correção Kruskal Matriz

--- Grafo_Trabalho_M2/Grafo_Trabalho_M2/GrafoMatriz.cpp
+++ Grafo_Trabalho_M2/Grafo_Trabalho_M2/GrafoMatriz.cpp
@@ -250,30 +250,17 @@ public:
 		for (int i = 0; i < vertices; i++)
 			key[i] = INT_MAX, mstSet[i] = false;
 
-		// Always include first 1st vertex in MST.  
-		// Make key 0 so that this vertex is picked as first vertex.  
+	
 		key[0] = 0;
-		parent[0] = -1; // First node is always root of MST  
+		parent[0] = -1;
 
-		// The MST will have V vertices  
 		for (int count = 0; count < vertices - 1; count++)
 		{
-			// Pick the minimum key vertex from the  
-			// set of vertices not yet included in MST  
 			int u = minKey(key, mstSet);
 
-			// Add the picked vertex to the MST Set  
 			mstSet[u] = true;
 
-			// Update key value and parent index of  
-			// the adjacent vertices of the picked vertex.  
-			// Consider only those vertices which are not  
-			// yet included in MST  
 			for (int v = 0; v < vertices; v++)
-
-				// graph[u][v] is non zero only for adjacent vertices of m  
-				// mstSet[v] is false for vertices not yet included in MST  
-				// Update the key only if graph[u][v] is smaller than key[v]  
 				if (grafoMatriz[u][v] && mstSet[v] == false && grafoMatriz[u][v] < key[v])
 					parent[v] = u, key[v] = grafoMatriz[u][v];
 		}
@@ -323,18 +310,27 @@ public:
 			for (int i = 0; i < vertices; i++) {
 				for (int j = 0; j < vertices; j++) {
 					if (find(i) != find(j) && grafoMatriz[i][j] < minim) {
-						mincost += minim;
+
+						if (grafoMatriz[i][j] != 0) // Se 500 for errado, tira esse IF
+							minim = grafoMatriz[i][j];
+
+						//mincost += minim;
 						a = i;
 						b = j;
+
+						//cout << "A: " << minim << endl;
 					}
 				}
 			}
 
-			//cout << minim << endl;
+			//cout << "B: " << minim << endl;
+			cout << minim << endl;
 
 			union1(a, b);
 			edge_count++;
 			mincost += minim;
+
+			minim = 0;
 		}
 
 		cout << "Custo minimo: " << mincost;
